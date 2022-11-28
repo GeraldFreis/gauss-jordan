27e3941d446a61ddf2716ad386621c1758e4a7ba
Gerald Freislich <freislichg@gmail.com>
2022-11-28 16:31:42 +1030

    safe commit

---
+++ testing/testing_matrix_functions.cpp
@@ -0,0 +1,26 @@
+#include "../Matrix.h"
+#include <iostream>
+#include <string>
+
+extern std::vector<int> string_to_numbers(std::string raw_input);
+
+
+int main(){
+    // Matrix m;
+    std::string raw_dimensions, raw_input;
+    std::cout << "What are the dimensions of the matrix?: (rows,columns)"  << "\n";
+    std::getline(std::cin, raw_dimensions, '\n');
+
+
+    std::cout << "Please enter the elements of the matrix row by row in a line: " << "\n";
+    std::getline(std::cin, raw_input, '\n');
+
+    std::vector<int> numbers = string_to_numbers(raw_input);
+
+    // for(auto a: numbers){
+    //     std::cout << a << " ";
+    // }
+    Matrix ma(numbers, raw_dimensions);
+    // ma.printElements();
+    return 0;
+}
