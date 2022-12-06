d5a591af594c9a598ee15ba9cd9f3faca24c257f
Gerald Freislich <freislichg@gmail.com>
2022-12-06 17:10:06 +1030

    newVersion because I needed to declutter ideas

--- Gauss_Jordan.cpp
+++ Gauss_Jordan.cpp
@@ -42,11 +42,42 @@ void GaussJordan::reduceColumn(int column, int pivot, int i){
 }
 
 void GaussJordan::testCombination(std::vector<std::string> current_combination){
-    // I want to interpret each step in the combination, pass this into another function which executes that step
     for(auto combination: current_combination){
         if(combination == "AM"){
-            
+            // iterate across each row and try to create a pivot
+            for(int c = 0; c < user_matrix.getDimensions().at(1); c++){
+                if(c < user_matrix.getDimensions().at(1)){
+
+                    int pivot = user_matrix.getEntry(c,c);
+
+                    for(int i = 0; i < user_matrix.getDimensions().at(0); i++){
+
+                        for(int scalar = -50; scalar < 50; scalar++){
+                            if(scalar != 0){
+                                if(scalar * pivot + user_matrix.getEntry(i,i) == 0){
+                                    user_matrix.addMultiple(c,i,scalar);
+                                    std::string command = std::to_string(scalar);
+                                    command += "AM";
+                                    commands.push(command);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }    
+            }
         }
+        else if(combination == "I"){
+            for(int row = 0; row < user_matrix.getDimensions().at(0); row++){
+                for(int column = 0; column < user_matrix.getDimensions().at(1); column++){
+                    if(row!=column && user_matrix.getEntry(row, column)!=0){
+                        user_matrix.swapRow(1,2);
+                    }
+                }
+            }
+        } else if(combination == "SM"){
+            continue;
+        }        
     }
 }
 
@@ -56,16 +87,17 @@ void GaussJordan::solve(){
     int rows = user_matrix.getDimensions().at(0); int columns = user_matrix.getDimensions().at(1);
     std::vector<std::vector<std::string>> all_combinations = Combinations(15);
     while(!all_combinations.empty() && !isReduced()){
-
+        testCombination(all_combinations.at(0));
+        all_combinations.erase(all_combinations.begin());
     }
-    for(int i = 1; i < columns; i++){
-        if(i-1 < columns){
-            int *current_row = user_matrix.getRow(i);
-            int pivot_value = current_row[i-1];
-            reduceColumn(i-1, pivot_value, i-1);
-            // std::cout << pivot_value << "\n";
-        } else {break;}
-    }
+    // for(int i = 1; i < columns; i++){
+    //     if(i-1 < columns){
+    //         int *current_row = user_matrix.getRow(i);
+    //         int pivot_value = current_row[i-1];
+    //         reduceColumn(i-1, pivot_value, i-1);
+    //         // std::cout << pivot_value << "\n";
+    //     } else {break;}
+    // }
 }
 
 void GaussJordan::printCommands(){
---
+++ newVersion/Matrix.cpp
@@ -0,0 +1,149 @@
+#include "Matrix.h"
+#include <iostream>
+
+Matrix::Matrix(int **raw_matrix, int size_x, int size_y){
+	if(size_x > 0 && size_y > 0){
+		user_matrix = new int*[size_x];
+		for(int i = 0; i < size_x; i++){
+			for(int j = 0; j < size_y; j++){
+				user_matrix[i][j] = raw_matrix[i][j];
+			}
+		}
+	}
+	rows = size_x;
+	columns = size_y;
+}
+
+Matrix::Matrix(){
+	user_matrix = new int*[10];
+
+	for(int i = 0; i < 0; i++){
+		user_matrix[i] = new int[10];
+	}
+
+	rows = 10; columns = 10;
+}
+
+int **Matrix::getMatrix(){
+	return user_matrix;
+};
+
+void Matrix::setMatrix(int **raw_matrix, int size_x, int size_y){
+	if(size_x > 0 && size_y > 0){
+		user_matrix = new int*[size_x];
+		for(int i = 0; i < size_x; i++){
+			for(int j = 0; j < size_y; j++){
+				user_matrix[i][j] = raw_matrix[i][j];
+			}
+		}
+	}
+	rows = size_x;
+	columns = size_y;
+};
+
+std::vector<int> Matrix::getDimensions(){
+	return std::vector<int> ({rows, columns});
+}
+
+
+int Matrix::getEntry(int x, int y){
+	return user_matrix[x][y];
+};
+
+void Matrix::printElements(){
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < columns; j++){
+			std::cout << user_matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+};
+
+int *Matrix::getRow(int row_number){
+	int *row = new int[columns];
+	for(int i = 0; i < columns; i++){
+		row[i] = user_matrix[row_number-1][i];
+	}
+	return row;
+};
+
+int *Matrix::getColumn(int column_number){
+	if(column_number > 0){
+		int *column = new int[rows];
+		for(int i = 0; i < rows; i++){
+			column[i] = user_matrix[i][column_number];
+		}
+		return column;
+	}
+	else {
+		return nullptr;
+	}
+};
+
+void Matrix::swapRow(int row_1, int row_2){
+	// iterate across the first row, store all values in a temporary array, make all values the second row
+	// iterate across second row, and make second row equal to temporary array values
+	if((row_1-1 >= 0 && row_1 < rows) && (row_2-1 >= 0 && row_2 < rows)){
+		int temporary_value = 0; 
+		for(int i = 0; i < columns; i++){
+			temporary_value = user_matrix[row_1-1][i];
+			user_matrix[row_1-1][i] = user_matrix[row_2-1][i];
+			user_matrix[row_2-1][i] = temporary_value;
+		}
+	}
+	else {
+		std::cout << "Out of row bounds (swapRow)" << "\n";
+	}
+
+};
+void Matrix::addMultiple(int row_to_add, int row_to_receive, int scalar){
+	if(row_to_add-1 >= 0 && row_to_add <= rows && row_to_receive-1>=0 && row_to_receive <= rows){
+		int value = 0;
+		for(int i = 0; i < columns; i++){
+			value = scalar * user_matrix[row_to_add-1][i];
+			user_matrix[row_to_receive-1][i] += value;
+		}
+	}
+};
+
+void Matrix::takeMultiple(int row_to_subtract, int row_to_receive, int scalar){
+	if(row_to_subtract-1 >= 0 && row_to_subtract <= rows && row_to_receive-1>=0 && row_to_receive <= rows){
+		int value = 0;
+		for(int i = 0; i < columns; i++){
+			value = scalar * user_matrix[row_to_subtract-1][i];
+			user_matrix[row_to_receive-1][i] -= value;
+		}
+	}
+};
+
+
+
+Matrix::Matrix(std::vector<int> m, std::string dim){
+	rows = dim.at(0) - '0'; columns = dim.at(2) - '0';
+	if(rows > 1 && columns > 1){
+		user_matrix = new int*[rows];
+		for(int i = 0; i < rows; i++){
+			user_matrix[i] = new int[columns];
+		}
+
+		bool greater_than_1 = true;
+
+		int counter_r = 0; int counter_c = 0; int vector_counter = 0;
+		for(auto a: m){
+			user_matrix[counter_r][counter_c] = a;
+			// std::cout << a << " " << counter_r << " " << counter_c << "\n";
+			vector_counter++; counter_c++;
+			if(counter_c % (columns) == 0){
+				counter_r++;
+				counter_c = 0;
+			}			
+		}
+	}
+	else {
+		std::cout << "matrix is not a matrix" << "\n";
+		user_matrix = new int*[10];
+		for(int i = 0; i < 10; i++){
+			user_matrix[i] = new int[10];
+		}
+	}
+};
